d2a061612a30982f1f3279d95481967885d9ef2b
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2018-11-06 22:19:51 -0500

    remove index.php from link

--- includes/ClicShopping/Sites/Shop/Pages/Account/Actions/OrderConditions.php
+++ includes/ClicShopping/Sites/Shop/Pages/Account/Actions/OrderConditions.php
@@ -1,11 +1,13 @@
 <?php
-/*
- * OrderConditions.php
- * @copyright Copyright 2008 - http://www.innov-concept.com
- * @Brand : ClicShopping(Tm) at Inpi all right Reserved
- * @license GPL 2 License & MIT Licencse
-
-*/
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *
+ *
+ */
   namespace ClicShopping\Sites\Shop\Pages\Account\Actions;
 
   use ClicShopping\OM\CLICSHOPPING;
@@ -14,12 +16,11 @@
   class OrderConditions extends \ClicShopping\OM\PagesActionsAbstract {
 
     public function execute() {
-      global $QconditionGeneralOfSales;
-
       $CLICSHOPPING_Customer = Registry::get('Customer');
-      $CLICSHOPPING_Db = Registry::get('Db');
       $CLICSHOPPING_NavigationHistory = Registry::get('NavigationHistory');
-      $CLICSHOPPING_Language = Registry::get('Language');
+      $CLICSHOPPING_Hooks = Registry::get('Hooks');
+
+      $CLICSHOPPING_Hooks->call('OrderConditions', 'PreAction');
 
       if (!$CLICSHOPPING_Customer->isLoggedOn()) {
         $CLICSHOPPING_NavigationHistory->setSnapshot();
@@ -28,37 +29,8 @@
 
 
       if (!isset($_GET['order_id']) || (isset($_GET['order_id']) && !is_numeric($_GET['order_id']))) {
-        CLICSHOPPING::redirect('account_history.php', '');
+        CLICSHOPPING::redirect('account_history.php');
       }
-
-      $QcustomerInfo = $CLICSHOPPING_Db->prepare('select o.customers_id
-                                            from :table_orders o,
-                                                 :table_orders_status s
-                                            where  o.orders_id = :orders_id
-                                            and o.orders_status = s.orders_status_id
-                                            and s.language_id = :language_id
-                                            and s.public_flag = :public_flag
-                                          ');
-
-      $QcustomerInfo->bindInt(':orders_id', $_GET['order_id']);
-      $QcustomerInfo->bindInt(':language_id', $CLICSHOPPING_Language->getId());
-      $QcustomerInfo->bindValue(':public_flag', '1');
-
-      $QcustomerInfo->execute();
-
-      $customer_id = $QcustomerInfo->valueInt('customers_id');
-
-// select the conditions of sales
-      $QconditionGeneralOfSales = $CLICSHOPPING_Db->prepare('select page_manager_general_condition
-                                                       from  :table_orders_pages_manager
-                                                       where orders_id = :orders_id
-                                                       and customers_id = :customers_id
-                                                     ');
-
-      $QconditionGeneralOfSales->bindInt(':orders_id', $_GET['order_id']);
-      $QconditionGeneralOfSales->bindInt(':customers_id', $customer_id);
-
-      $QconditionGeneralOfSales->execute();
 
       $this->page->setFile('order_conditions.php');
     }
--- includes/ClicShopping/Sites/Shop/Pages/Account/templates/order_conditions.php
+++ includes/ClicShopping/Sites/Shop/Pages/Account/templates/order_conditions.php
@@ -1,15 +1,33 @@
 <?php
-/*
- * order_condition.php
- * @copyright Copyright 2008 - http://www.innov-concept.com
- * @Brand : ClicShopping(Tm) at Inpi all right Reserved
- * @license GPL 2 License & MIT Licencse
-
-*/
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *
+ *
+ */
 
   use ClicShopping\OM\Registry;
 
   $CLICSHOPPING_Page = Registry::get('Site')->getPage();
+  $CLICSHOPPING_Language = Registry::get('Language');
+  $CLICSHOPPING_Db = Registry::get('Db');
+  $CLICSHOPPING_Customer = Registry::get('Customer');
+
+  $customer_id = $CLICSHOPPING_Customer->getID();
+
+  $QconditionGeneralOfSales = $CLICSHOPPING_Db->prepare('select page_manager_general_condition
+                                                             from  :table_orders_pages_manager
+                                                             where orders_id = :orders_id
+                                                             and customers_id = :customers_id
+                                                           ');
+
+  $QconditionGeneralOfSales->bindInt(':orders_id', $_GET['order_id']);
+  $QconditionGeneralOfSales->bindInt(':customers_id', $customer_id);
+
+  $QconditionGeneralOfSales->execute();
 ?>
 
 <html dir="ltr" lang="fr">
