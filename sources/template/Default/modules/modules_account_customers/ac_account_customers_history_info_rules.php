2859452f2698b424b5df11aee118e404e8f613f6
ClicShopping <Yae8mr2o*>
2020-08-29 16:06:26 +0200

    update

--- sources/template/Default/modules/modules_account_customers/ac_account_customers_history_info_rules.php
+++ sources/template/Default/modules/modules_account_customers/ac_account_customers_history_info_rules.php
@@ -17,10 +17,10 @@
 
     public $code;
     public $group;
-    public $title;
-    public $description;
-    public $sort_order;
-    public $enabled = false;
+    public string $title;
+    public string $description;
+    public ?int $sort_order = 0;
+    public bool $enabled = false;
 
     public function __construct() {
       $this->code = get_class($this);
@@ -40,7 +40,7 @@
       $CLICSHOPPING_Order = Registry::get('Order');
       $CLICSHOPPING_Customer = Registry::get('Customer');
 
-      if (isset($_GET['Account']) &&  isset($_GET['HistoryInfo']) ) {
+      if (isset($_GET['Account']) &&  isset($_GET['HistoryInfo'])) {
 
         $content_width = (int)MODULE_ACCOUNT_CUSTOMERS_HISTORY_INFO_RULES_CONTENT_WIDTH;
 
